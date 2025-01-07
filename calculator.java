2d3289c615f22376628a1d9cd789c16e6a16aac5
Deepanshu malviya <deepanshumalviya58@gmail.com>
2025-01-07 12:50:47 +0530

    finishing code added

--- calculator.java
+++ calculator.java
@@ -13,6 +13,32 @@ public class calculator{
 	public static float division(float a,float b){
 		return a/b;
 	}
+	public static void answer(float a,float b){
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("choose an operation:\npress 1 for addition,\n2 for substraction,\n3 for multiplication,\n4 for division ");
+		int choice=sc.nextInt();
+		switch(choice){
+			case 1:{
+				System.out.println(addition(a,b));
+				break;
+			}
+			case 2:{
+				System.out.println(substraction(a,b));
+				break;
+			}
+			case 3:{
+				System.out.println(multiplication(a, b));
+				break;
+			}
+			case 4:{
+				System.out.println(division(a, b));
+				break;
+			}
+			default:
+			System.out.println("wrong choice...");
+		}
+	}
 	public static void main(String []args){
 		System.out.println("welcome to calculator");
 		Scanner sc=new Scanner(System.in);
@@ -22,6 +48,6 @@ public class calculator{
 		System.out.println("Enter 2nd number:");
 		
 		b=sc.nextFloat();
-	
+		
 	}
 }
