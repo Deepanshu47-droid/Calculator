fef0d7bd0e96ecacdcd17a1155cd1a1113ac27a0
Deepanshu malviya <deepanshumalviya58@gmail.com>
2025-01-07 12:25:35 +0530

    division functionality added

--- calculator.java
+++ calculator.java
@@ -10,7 +10,9 @@ public class calculator{
 	public static float multiplication(float a,float b){
 		return a*b;
 	}
-
+	public static float division(float a,float b){
+		return a/b;
+	}
 	public static void main(String []args){
 		System.out.println("welcome to calculator");
 		Scanner sc=new Scanner(System.in);
