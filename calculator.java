3a4a46bc5f7419c1df96c6289bf027ab709bd114
Deepanshu malviya <deepanshumalviya58@gmail.com>
2025-01-07 11:55:11 +0530

    calculator added

---
+++ calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class calculator{
+	
+	public static void main(String []args){
+		System.out.println("welcome to calculator");
+		Scanner sc=new Scanner(System.in);
+		float a,b;
+		System.out.println("Enter 1st number:");
+		a=sc.nextFloat();
+		System.out.println("Enter 2nd number:");
+		
+		b=sc.nextFloat();
+	
+	}
+}
